a9df91e50d03436a223e160f42290ee622155b54
PrgPavlos <84491567+PrgPavlos@users.noreply.github.com>
2021-05-26 19:33:24 +0200

    First Version

---
+++ pexeso 2.0/Class1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pexeso_2._0
+{
+    class Hra
+    {
+
+        public int x1;
+        public int y1;
+        public int x2;
+        public int y2;
+
+
+        public string[,] pexesoSkryte = {
+            { "X", "X", "X", "X" },
+            { "X", "X", "X", "X" },
+            { "X", "X", "X", "X" },
+            { "X", "X", "X", "X" }
+        };
+
+        public string[,] pexesoOdkryte = {
+                { "A", "A", "B", "B" },
+                { "C", "C", "D", "D" },
+                { "E", "E", "F", "F"},
+                { "G", "G", "H", "H" }
+        };
+
+        
+        public void VýpisHRY()
+        {
+            for (int j = 0; j < pexesoOdkryte.GetLength(1); j++)
+            {
+                for (int i = 0; i < pexesoOdkryte.GetLength(0); i++)
+                {
+                    Console.Write(pexesoSkryte[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void Zamichani()
+        {
+            int opakovani = 4;
+            int opakovani_ = 0;
+            
+            string[] karty = { "A", "B", "C", "D", "E", "F", "G", "H", "A", "B", "C", "D", "E", "F", "G", "H" };
+            List<int> pozice_list = new List<int>();
+            Random random = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                while (opakovani_ < 4)
+                {
+                    for (int l = 0; l < opakovani; l++)
+                    {
+                        int pozice = random.Next(0, 16);
+                        if (!(pozice_list.Contains(pozice)))
+                        {
+
+                            pozice_list.Add(pozice);
+                            pexesoOdkryte[i, opakovani_] = karty[pozice];
+                            opakovani_++;
+
+
+                        }
+                        else
+                        {
+                            opakovani++;
+                        }
+                    }
+                }
+
+            }
+        }
+
+        
+
+
+
+    }
+}
---
+++ pexeso 2.0/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pexeso_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hra pexeso = new Hra();
+
+
+            string PrvniKarta = "0";
+            string DruhaKarta = "0";
+
+
+
+          
+
+
+
+            while (1 > 0)
+            {
+                for (int i = 0; i < 1; i++)
+                {
+
+                    Console.Write("Zadejte první souřadnici X: ");
+                    pexeso.x1 = Convert.ToInt32(Console.ReadLine());
+
+                    for (int j = 0; j < 1; j++)
+                    {
+                        Console.Write("Zadejte druhou souřadnici Y:");
+                        pexeso.y1 = Convert.ToInt32(Console.ReadLine());
+
+                        pexeso.pexesoSkryte[pexeso.x1 - 1, pexeso.y1 - 1] = pexeso.pexesoOdkryte[pexeso.x1 - 1, pexeso.y1 - 1];
+                        Console.Clear();
+                        pexeso.VýpisHRY();
+
+                        PrvniKarta = pexeso.pexesoOdkryte[pexeso.x1 - 1, pexeso.y1 - 1];
+
+
+                    }
+                }
+                for (int i = 0; i < 1; i++)
+                {
+                    Console.Write("Zadejte první souřadnici X: ");
+                    pexeso.x2 = Convert.ToInt32(Console.ReadLine());
+
+                    for (int j = 0; j < 1; j++)
+                    {
+                        Console.Write("Zadejte druhou souřadnici Y:");
+                        pexeso.y2 = Convert.ToInt32(Console.ReadLine());
+
+                        pexeso.pexesoSkryte[pexeso.x2 - 1, pexeso.y2 - 1] = pexeso.pexesoOdkryte[pexeso.x2 - 1, pexeso.y2 - 1];
+                        Console.Clear();
+                        pexeso.VýpisHRY();
+
+                        DruhaKarta = pexeso.pexesoOdkryte[pexeso.x2 - 1, pexeso.y2 - 1];
+
+                    }
+
+                }
+                Console.ReadKey();
+
+
+                if (!(PrvniKarta == DruhaKarta))
+                {
+                    pexeso.pexesoSkryte[pexeso.x1 - 1, pexeso.y1 - 1] = "X";
+                    pexeso.pexesoSkryte[pexeso.x2 - 1, pexeso.y2 - 1] = "X";
+                    Console.Clear();
+                    pexeso.VýpisHRY();
+
+
+                }
+            }
+
+            Console.ReadKey();
+                
+            
+        }
+    }
+}
